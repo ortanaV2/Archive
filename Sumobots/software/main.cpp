c40d6e2cf9c787a15ca8265b2e898f537276ad05
Ortana <126905156+ortanaV2@users.noreply.github.com>
2025-06-01 23:41:31 +0200

    Update main.cpp

--- Sumobots/software/main.cpp
+++ Sumobots/software/main.cpp
@@ -1,22 +1,25 @@
-// Motor-Pins
-const int MOTOR_LEFT_FORWARD = 2;
-const int MOTOR_LEFT_BACKWARD = 0;
-const int MOTOR_RIGHT_FORWARD = 4;
-const int MOTOR_RIGHT_BACKWARD = 5;
+const int MOTOR_LEFT_FORWARD = 5;
+const int MOTOR_LEFT_BACKWARD = 4;
+const int MOTOR_RIGHT_FORWARD = 2;
+const int MOTOR_RIGHT_BACKWARD = 0;
 
-// Sensors
 const int SENSOR_RIGHT = 12;
 const int SENSOR_LEFT = 14;
 
+// global speed (0-255)
+const int motor_speed = 230;
+const int left_motor_calib = 7;
+const int right_motor_calib = 0;
+
 int right_line;
 int left_line;
+bool isMovingForward = false;
+bool wasOnBothLines = false;
 
 void setup() {
-  // Sensor setup
   pinMode(SENSOR_RIGHT, INPUT_PULLUP);
   pinMode(SENSOR_LEFT, INPUT_PULLUP);
 
-  // Motor setup
   pinMode(MOTOR_LEFT_FORWARD, OUTPUT);
   pinMode(MOTOR_LEFT_BACKWARD, OUTPUT);
   pinMode(MOTOR_RIGHT_FORWARD, OUTPUT);
@@ -26,76 +29,115 @@ void setup() {
   freeze_all();
 }
 
-// Alle Motoren stoppen
 void freeze_all() {
-  digitalWrite(MOTOR_LEFT_FORWARD, LOW);
-  digitalWrite(MOTOR_LEFT_BACKWARD, LOW);
-  digitalWrite(MOTOR_RIGHT_FORWARD, LOW);
-  digitalWrite(MOTOR_RIGHT_BACKWARD, LOW);
+  analogWrite(MOTOR_LEFT_FORWARD, 0);
+  analogWrite(MOTOR_LEFT_BACKWARD, 0);
+  analogWrite(MOTOR_RIGHT_FORWARD, 0);
+  analogWrite(MOTOR_RIGHT_BACKWARD, 0);
 }
 
-// Sichere Einzelsteuerung für linken Motor
 void setLeftMotor(bool forward, bool enable) {
-  digitalWrite(MOTOR_LEFT_FORWARD, (forward && enable) ? HIGH : LOW);
-  digitalWrite(MOTOR_LEFT_BACKWARD, (!forward && enable) ? HIGH : LOW);
+  if (forward && enable) {
+    analogWrite(MOTOR_LEFT_FORWARD, motor_speed - left_motor_calib);
+    analogWrite(MOTOR_LEFT_BACKWARD, 0);
+  } else if (!forward && enable) {
+    analogWrite(MOTOR_LEFT_FORWARD, 0);
+    analogWrite(MOTOR_LEFT_BACKWARD, motor_speed - left_motor_calib);
+  } else {
+    analogWrite(MOTOR_LEFT_FORWARD, 0);
+    analogWrite(MOTOR_LEFT_BACKWARD, 0);
+  }
 }
 
-// Sichere Einzelsteuerung für rechten Motor
 void setRightMotor(bool forward, bool enable) {
-  digitalWrite(MOTOR_RIGHT_FORWARD, (forward && enable) ? HIGH : LOW);
-  digitalWrite(MOTOR_RIGHT_BACKWARD, (!forward && enable) ? HIGH : LOW);
+  if (forward && enable) {
+    analogWrite(MOTOR_RIGHT_FORWARD, motor_speed - right_motor_calib);
+    analogWrite(MOTOR_RIGHT_BACKWARD, 0);
+  } else if (!forward && enable) {
+    analogWrite(MOTOR_RIGHT_FORWARD, 0);
+    analogWrite(MOTOR_RIGHT_BACKWARD, motor_speed - right_motor_calib);
+  } else {
+    analogWrite(MOTOR_RIGHT_FORWARD, 0);
+    analogWrite(MOTOR_RIGHT_BACKWARD, 0);
+  }
 }
 
-// Kombinationen
-void driveForward(int time) {
+void driveForward() {
   setLeftMotor(true, true);
   setRightMotor(true, true);
-  delay(time);
-  freeze_all();
+  isMovingForward = true;
 }
 
-void driveBackward(int time) {
+void driveBackward() {
   setLeftMotor(false, true);
   setRightMotor(false, true);
-  delay(time);
-  freeze_all();
+  isMovingForward = false;
 }
 
-void rotateLeft(int time) {
+void stopMoving() {
+  freeze_all();
+  isMovingForward = false;
+}
+
+void rotateLeft() {
   setLeftMotor(false, true);
   setRightMotor(true, true);
-  delay(time);
-  freeze_all();
 }
 
-void rotateRight(int time) {
+void rotateRight() {
   setLeftMotor(true, true);
   setRightMotor(false, true);
-  delay(time);
-  freeze_all();
 }
 
 void loop() {
   right_line = digitalRead(SENSOR_RIGHT);
   left_line = digitalRead(SENSOR_LEFT);
 
+  Serial.print("L: ");
+  Serial.print(left_line);
+  Serial.print(" R: ");
+  Serial.println(right_line);
+
   if (left_line == HIGH && right_line == LOW) {
-    rotateRight(250);
-    delay(10);
-  }
-
+    stopMoving();
+    driveBackward();
+    delay(300);
+    stopMoving();
+    rotateRight();
+    delay(200);
+    freeze_all();
+  } 
   else if (right_line == HIGH && left_line == LOW) {
-    rotateLeft(250);
-    delay(10);
-  }
-
+    stopMoving();
+    driveBackward();
+    delay(300);
+    stopMoving();
+    rotateLeft();
+    delay(200);
+    freeze_all();
+  } 
   else if (right_line == HIGH && left_line == HIGH) {
-    rotateRight(500);
-    delay(10);
+    driveBackward();
+    delay(300);
+    freeze_all();
+    wasOnBothLines = true;
+  } 
+  else {
+    if (wasOnBothLines) {
+      stopMoving();
+      if (random(0, 2) == 0) {
+        rotateLeft();
+      } else {
+        rotateRight();
+      }
+      delay(300);
+      freeze_all();
+      wasOnBothLines = false;
+    }
+    if (!isMovingForward) {
+      driveForward();
+    }
   }
 
-  else {
-    driveForward(50);
-    delay(10);
-  }
+  delay(10);
 }
