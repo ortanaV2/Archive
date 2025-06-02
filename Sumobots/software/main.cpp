26677d622e8ab37b15b392d5cf6ad5b356484479
Ortana <126905156+ortanaV2@users.noreply.github.com>
2025-06-02 22:02:27 +0200

    Update: Reverse Current Breaking & Improved Movement

--- Sumobots/software/main.cpp
+++ Sumobots/software/main.cpp
@@ -6,25 +6,22 @@ const int MOTOR_RIGHT_BACKWARD = 0;
 const int SENSOR_RIGHT = 12;
 const int SENSOR_LEFT = 14;
 
-// global speed (0-255)
-const int motor_speed = 230;
-const int left_motor_calib = 7;
-const int right_motor_calib = 0;
+int MOTOR_SPEED = 230;
+const float LEFT_MOTOR_CALIB = 3.75;
+const float RIGHT_MOTOR_CALIB = 0;
 
-int right_line;
-int left_line;
-bool isMovingForward = false;
-bool wasOnBothLines = false;
+int RIGHT_LINE;
+int LEFT_LINE;
+bool IS_MOVING_FORWARD = false;
 
 void setup() {
+  analogWriteFreq(10000);
   pinMode(SENSOR_RIGHT, INPUT_PULLUP);
   pinMode(SENSOR_LEFT, INPUT_PULLUP);
-
   pinMode(MOTOR_LEFT_FORWARD, OUTPUT);
   pinMode(MOTOR_LEFT_BACKWARD, OUTPUT);
   pinMode(MOTOR_RIGHT_FORWARD, OUTPUT);
   pinMode(MOTOR_RIGHT_BACKWARD, OUTPUT);
-
   Serial.begin(9600);
   freeze_all();
 }
@@ -34,15 +31,24 @@ void freeze_all() {
   analogWrite(MOTOR_LEFT_BACKWARD, 0);
   analogWrite(MOTOR_RIGHT_FORWARD, 0);
   analogWrite(MOTOR_RIGHT_BACKWARD, 0);
+  IS_MOVING_FORWARD = false;
+}
+
+void brake_all() {
+  analogWrite(MOTOR_LEFT_FORWARD, 255);
+  analogWrite(MOTOR_LEFT_BACKWARD, 255);
+  analogWrite(MOTOR_RIGHT_FORWARD, 255);
+  analogWrite(MOTOR_RIGHT_BACKWARD, 255);
+  IS_MOVING_FORWARD = false;
 }
 
 void setLeftMotor(bool forward, bool enable) {
   if (forward && enable) {
-    analogWrite(MOTOR_LEFT_FORWARD, motor_speed - left_motor_calib);
+    analogWrite(MOTOR_LEFT_FORWARD, MOTOR_SPEED - LEFT_MOTOR_CALIB);
     analogWrite(MOTOR_LEFT_BACKWARD, 0);
   } else if (!forward && enable) {
     analogWrite(MOTOR_LEFT_FORWARD, 0);
-    analogWrite(MOTOR_LEFT_BACKWARD, motor_speed - left_motor_calib);
+    analogWrite(MOTOR_LEFT_BACKWARD, MOTOR_SPEED - LEFT_MOTOR_CALIB);
   } else {
     analogWrite(MOTOR_LEFT_FORWARD, 0);
     analogWrite(MOTOR_LEFT_BACKWARD, 0);
@@ -51,11 +57,11 @@ void setLeftMotor(bool forward, bool enable) {
 
 void setRightMotor(bool forward, bool enable) {
   if (forward && enable) {
-    analogWrite(MOTOR_RIGHT_FORWARD, motor_speed - right_motor_calib);
+    analogWrite(MOTOR_RIGHT_FORWARD, MOTOR_SPEED - RIGHT_MOTOR_CALIB);
     analogWrite(MOTOR_RIGHT_BACKWARD, 0);
   } else if (!forward && enable) {
     analogWrite(MOTOR_RIGHT_FORWARD, 0);
-    analogWrite(MOTOR_RIGHT_BACKWARD, motor_speed - right_motor_calib);
+    analogWrite(MOTOR_RIGHT_BACKWARD, MOTOR_SPEED - RIGHT_MOTOR_CALIB);
   } else {
     analogWrite(MOTOR_RIGHT_FORWARD, 0);
     analogWrite(MOTOR_RIGHT_BACKWARD, 0);
@@ -65,79 +71,64 @@ void setRightMotor(bool forward, bool enable) {
 void driveForward() {
   setLeftMotor(true, true);
   setRightMotor(true, true);
-  isMovingForward = true;
+  IS_MOVING_FORWARD = true;
 }
 
-void driveBackward() {
+void driveBackward(int speed) {
+  int previous = MOTOR_SPEED;
+  MOTOR_SPEED = speed;
   setLeftMotor(false, true);
   setRightMotor(false, true);
-  isMovingForward = false;
-}
-
-void stopMoving() {
-  freeze_all();
-  isMovingForward = false;
+  IS_MOVING_FORWARD = false;
+  MOTOR_SPEED = previous;
 }
 
 void rotateLeft() {
   setLeftMotor(false, true);
   setRightMotor(true, true);
+  IS_MOVING_FORWARD = false;
 }
 
 void rotateRight() {
   setLeftMotor(true, true);
   setRightMotor(false, true);
+  IS_MOVING_FORWARD = false;
+}
+
+void rotateUntilNoLine(bool leftDir) {
+  if (leftDir) {
+    rotateLeft();
+  } else {
+    rotateRight();
+  }
+  while (digitalRead(SENSOR_LEFT) == HIGH || digitalRead(SENSOR_RIGHT) == HIGH) {
+    delay(10);
+  }
+  delay(50);
+  freeze_all();
 }
 
 void loop() {
-  right_line = digitalRead(SENSOR_RIGHT);
-  left_line = digitalRead(SENSOR_LEFT);
+  RIGHT_LINE = digitalRead(SENSOR_RIGHT);
+  LEFT_LINE = digitalRead(SENSOR_LEFT);
 
-  Serial.print("L: ");
-  Serial.print(left_line);
-  Serial.print(" R: ");
-  Serial.println(right_line);
-
-  if (left_line == HIGH && right_line == LOW) {
-    stopMoving();
-    driveBackward();
-    delay(300);
-    stopMoving();
-    rotateRight();
-    delay(200);
-    freeze_all();
-  } 
-  else if (right_line == HIGH && left_line == LOW) {
-    stopMoving();
-    driveBackward();
-    delay(300);
-    stopMoving();
-    rotateLeft();
-    delay(200);
-    freeze_all();
-  } 
-  else if (right_line == HIGH && left_line == HIGH) {
-    driveBackward();
+  if (LEFT_LINE == HIGH && RIGHT_LINE == LOW) {
+    driveBackward(255);
     delay(300);
     freeze_all();
-    wasOnBothLines = true;
-  } 
-  else {
-    if (wasOnBothLines) {
-      stopMoving();
-      if (random(0, 2) == 0) {
-        rotateLeft();
-      } else {
-        rotateRight();
-      }
-      delay(300);
-      freeze_all();
-      wasOnBothLines = false;
-    }
-    if (!isMovingForward) {
-      driveForward();
-    }
+    rotateUntilNoLine(false);
+  } else if (RIGHT_LINE == HIGH && LEFT_LINE == LOW) {
+    driveBackward(255);
+    delay(300);
+    freeze_all();
+    rotateUntilNoLine(true);
+  } else if (RIGHT_LINE == HIGH && LEFT_LINE == HIGH) {
+    driveBackward(255);
+    delay(300);
+    freeze_all();
+  } else {
+    driveForward();
   }
 
-  delay(10);
+  delay(5);
 }
